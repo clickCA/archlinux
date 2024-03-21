753d283a8176e0dc4067876357b6559c9f4249fd
clickCA <chanakorn.aramsak@gmail.com>
2024-03-21 16:35:28 +0700

    add starter app

---
+++ starter-app.sh
@@ -0,0 +1,19 @@
+sudo pacman -Sy git
+cd Downloads
+git clone https://aur.archlinux.org/google-chrome.git
+cd google-chrome
+makepkg -si
+cd ..
+git clone https://aur.archlinux.org/visual-studio-code-bin.git
+cd visual-studio-code-bin
+makepkg -si
+cd ..
+pacman -Sy discord 
+git clone https://aur.archlinux.org/telegram-desktop-bin.git
+cd telegram-desktop-bin
+makepkg -si
+cd ..
+git clone https://aur.archlinux.org/notion-app.git 
+cd notion-app
+makepkg -si
+cd ..
