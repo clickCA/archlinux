a95d05e94f8f01f2a7f7efd4cdab46cb06541915
Chanakorn Aramsak <chanakorn.aramsak@gmail.com>
2024-09-02 14:48:06 +0700

    Fix bug cant find node

--- application/node.sh
+++ application/node.sh
@@ -7,13 +7,20 @@ sudo pacman -Syu
 sudo pacman -S git
 
 # Clone the NVM repository
-wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 
-
+nvm install --lts
 # Verify installation
 node -v
 npm -v
+# Install unzip
+sudo pacman -S unzip
+# Install bun
+curl -fsSL https://bun.sh/install | bash
 
+# Install yarn
 npm install -g yarn
 
 echo "Node.js LTS version $(node -v) and npm are installed!"
